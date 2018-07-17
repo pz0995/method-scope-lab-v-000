3335668c2f8d7c431e527d38495c7a2fe6cf0a3f
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-17 08:19:46 +0000

    automatically backed up by learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/catch_phrase.rb"
 
 describe "#catch_phrase" do
   it "puts out a catch phrase" do
-    phrase = "It's-a me, Mario!"
+    phrase = "It's-a me, Mario"
     expect{catch_phrase(phrase)}.to output("It's-a me, Mario").to_stdout
   end
 end
